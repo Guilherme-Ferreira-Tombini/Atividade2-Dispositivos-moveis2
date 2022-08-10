a235cac63b382d5a825dc1dde47ec1bcc2b356f8
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2022-08-10 18:29:18 -0300

    modificações

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-window.onload = () => {
-    "use strict";
-    if("serviceWorker" in navigator){
-        navigator.serviceWorker.register("./sw.js");
-    }
-};
+// window.onload = () => {
+//     "use strict";
+//     if("serviceWorker" in navigator){
+//         navigator.serviceWorker.register("./sw.js");
+//     }
+// };
+
+function guarani(){
+    let guarani = document.getElementById('guarani').value;
+    let calculo = "O resultado da área é " + Number(guarani * 0.00074);
+    document.getElementById('result').textContent = calculo;
+}
+
+function dolar(){
+    let dolar = document.getElementById('dolar').value;
+    let calculo = "O resultado da área é " + Number(dolar * 5.09);
+    document.getElementById('result').textContent = calculo;
+}
