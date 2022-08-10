794ea5d6420d6095b856d72d3a90121eb5db4794
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2022-08-10 19:04:19 -0300

    Adicionado estilização aos botões e para o texto de resultado

--- js/main.js
+++ js/main.js
@@ -7,12 +7,12 @@
 
 function guarani(){
     let guarani = document.getElementById('guarani').value;
-    let calculo = "O resultado da área é " + Number(guarani * 0.00074);
+    let calculo = "A conversão entre guarani para reais é equivalente a R$" + Number(guarani * 0.00074);
     document.getElementById('result').textContent = calculo;
 }
 
 function dolar(){
     let dolar = document.getElementById('dolar').value;
-    let calculo = "O resultado da área é " + Number(dolar * 5.09);
+    let calculo = "A conversão entre dolares para reais é equivalente a R$" + Number(dolar * 5.09);
     document.getElementById('result').textContent = calculo;
 }
